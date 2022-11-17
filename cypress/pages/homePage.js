5128eb9389481339d98bad34a0cde4a5f7194ffb
fmora <fmora@cmnhospitals.org>
2022-11-16 21:09:54 -0600

    page object model implemented

---
+++ cypress/pages/homePage.js
@@ -0,0 +1,15 @@
+
+class homePage {
+
+    elements = {
+        loginBtn: () => cy.get("#signin"),
+        logOffBtn: () => cy.get("#logout")
+    }
+
+    clickOnSignin() {
+        this.elements.loginBtn().click()
+    }
+}
+
+
+module.exports = new homePage();
